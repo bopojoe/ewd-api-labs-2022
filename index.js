45aa0b833d09e937ac4fec337e6d1e8dfbdafe99
James O'Rourke <devbopo.joe@gmail.com>
2022-05-17 11:48:23 +0100

    added routing for movie and reviews

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ dotenv.config();
 const app = express();
 
 const port = process.env.PORT;
-
+app.use(express.json());
 app.use("/api/movies", moviesRouter);
+
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
