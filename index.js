8611724031c46f588641ff6618b80db3b01b3b66
James O'Rourke <devbopo.joe@gmail.com>
2022-05-17 12:49:52 +0100

    Added Accounts Endpoints

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import createAccountsRouter from "./src/accounts/routes";
+import buildDependencies from "./src/config/dependencies";
 import dotenv from "dotenv";
 import express from "express";
 import moviesRouter from "./src/movies";
@@ -6,12 +8,14 @@ import genresRouter from "./src/genres";
 dotenv.config();
 
 const app = express();
+const dependencies = buildDependencies();
+
 
 const port = process.env.PORT;
 app.use(express.json());
 app.use("/api/movies", moviesRouter);
 app.use("/api/genres", genresRouter);
-
+app.use("/api/accounts", createAccountsRouter(dependencies));
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
